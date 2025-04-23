da5132379fe083e10547cf5c98f94c2d17ba02fd
ankaichen <2751551364akc@gmail.com>
2025-04-23 13:28:40 +0800

    modify task 59

--- cpp/task/impl/task59.hpp
+++ cpp/task/impl/task59.hpp
@@ -27,9 +27,11 @@ public:
 };
 
 Task59::Task59() {
-    this->addTestCase("n = 3", "[[1,2,3],[8,9,4],[7,6,5]]");
-    this->addTestCase("n = 2", "[[1,2],[4,3]]");
     this->addTestCase("n = 1", "[[1]]");
+    this->addTestCase("n = 2", "[[1,2],[4,3]]");
+    this->addTestCase("n = 3", "[[1,2,3],[8,9,4],[7,6,5]]");
+//    this->addTestCase("n = 4", "[[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]");
+//    this->addTestCase("n = 5", "[[1,2,3,4,5],[16,17,18,19,6],[15,24,25,20,7],[14,23,22,21,8],[13,12,11,10,9]]");
 }
 
 enum class Direction {
@@ -97,6 +99,7 @@ std::vector<std::vector<int>> generateMatrix2(int n) {
             result[i][j] = value++;
         ++startI;
         ++startJ;
+        --edgeLen;
         --loop;
     }
     if (n % 2 == 1) { // 奇数计算中心点
