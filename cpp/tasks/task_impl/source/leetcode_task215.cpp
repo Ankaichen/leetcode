d9426bfb889a0bb404a1fb9126fe223430579326
ankaichen <2751551364akc@gmail.com>
2025-08-07 00:32:19 +0800

    add leetcode task215

--- cpp/tasks/task_impl/source/leetcode_task215.cpp
+++ cpp/tasks/task_impl/source/leetcode_task215.cpp
@@ -10,13 +10,13 @@
 
 #include "../include/leetcode_task215.h"
 
-static int findKthLargest1(std::vector<int>& nums, int k) {
+static int findKthLargest1(std::vector<int> &nums, int k) {
     // 不满足时间复杂度 O(n)
     std::sort(nums.begin(), nums.end());
     return nums[nums.size() - k];
 }
 
-static int partition(std::vector<int>& nums, int left, int right) {
+static int partition(std::vector<int> &nums, int left, int right) {
     std::swap(nums[left], nums[left + (right - left) / 2]);  // 将中间值作为枢轴 避免快排最坏情况
     int pivot = nums[left];
     while (left < right) {
@@ -33,7 +33,7 @@ static int partition(std::vector<int>& nums, int left, int right) {
     return left;
 }
 
-static int findKthLargest2(std::vector<int>& nums, int k) {
+static int findKthLargest2(std::vector<int> &nums, int k) {
     // 快排思路 每次确定一个位置 目标范围减少一半 直到找到目标位置
     // 时间复杂度 O(nlogk)
     int left = 0, right = nums.size() - 1;
@@ -48,36 +48,38 @@ static int findKthLargest2(std::vector<int>& nums, int k) {
     return nums[target];
 }
 
-static void adjustHeap(std::vector<int>& heap, int from = 1) {
-    --from;
-    for (int i = 2, k = 1; from + i < heap.size(); i = k * 2) {
-        if (from + i + 1 < heap.size() && heap[from + i] < heap[from + i + 1]) i = i + 1;
-        if (heap[from + i] < heap[from + k]) std::swap(heap[from + i], heap[from + k]);
-        k = i;
+static void adjustHeap(std::vector<int> &heap, int start) {
+    for (int i = start, left = i * 2; left < heap.size(); left = i * 2) {
+        if (left + 1 < heap.size() && heap[left] > heap[left + 1]) left = left + 1;
+        if (heap[left] < heap[i]) {
+            std::swap(heap[left], heap[i]);
+            i = left;
+        } else break;
     }
 }
 
-static void initHeap(std::vector<int>& heap, int from) {
-    for (int i = heap.size() - 1; i >= from; --i) {
+static void initHeap(std::vector<int> &heap) {
+    for (int i = heap.size() / 2; i >= 1; --i) {
         adjustHeap(heap, i);
     }
 }
 
-static int findKthLargest3(std::vector<int>& nums, int k) {
+static int findKthLargest3(std::vector<int> &nums, int k) {
     // 堆排思路 维护一个大小为k的小根堆 遍历数组 不断替换小根堆中的最小值
     // 时间复杂度 O(nlogk)
     int target = nums.size() - k;
-    initHeap(nums, target);
-    std::cout << target << std::endl;
-    for (auto i : nums) std::cout << i << ", ";
-    std::cout << std::endl;
+    std::vector<int> heap(1, 0);
+    heap.insert(heap.begin() + 1, nums.begin() + target, nums.end());
+    initHeap(heap);
     for (int i = 0; i < target; ++i) {
-        if (nums[i] > nums[target]) {
-            std::swap(nums[i], nums[target]);
-            adjustHeap(nums, target);
+        if (nums[i] > heap[1]) {
+            heap[1] = nums[i];
+            adjustHeap(heap, 1);
         }
     }
-    return nums[target];
+    return heap[1];
 }
 
-int LeetcodeTask215::solve(const std::vector<int>& nums, int k) const { return findKthLargest3(const_cast<std::vector<int>&>(nums), k); }
+int LeetcodeTask215::solve(const std::vector<int> &nums, int k) const {
+    return findKthLargest3(const_cast<std::vector<int> &>(nums), k);
+}
