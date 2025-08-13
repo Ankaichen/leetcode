91743fbb998c7d27d46b054cb70decffa5bc52c1
Ankaichen <2751551364akc@gmail.com>
2025-08-13 16:15:22 +0800

    review leetcode task 25

--- cpp/tasks/task_impl/source/leetcode_task25.cpp
+++ cpp/tasks/task_impl/source/leetcode_task25.cpp
@@ -1,16 +1,16 @@
 /**
-  ******************************************************************************
-  * @file           : leetcode_task25.cpp
-  * @author         : An Kaichen
-  * @brief          : None
-  * @attention      : None
-  * @date           : 25-8-6
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : leetcode_task25.cpp
+ * @author         : An Kaichen
+ * @brief          : None
+ * @attention      : None
+ * @date           : 25-8-6
+ ******************************************************************************
+ */
 
 #include "../include/leetcode_task25.h"
 
-static ListNode *reverse(ListNode *head, int k) { // head 为头节点 反转head后的k个节点
+static ListNode *reverse(ListNode *head, int k) {  // head 为头节点 反转head后的k个节点
     if (!head) return nullptr;
     ListNode *p = head->next, *last = head->next;
     while (p && k) {
@@ -21,10 +21,10 @@ static ListNode *reverse(ListNode *head, int k) { // head 为头节点 反转hea
         --k;
     }
     if (last) last->next = p;
-    return last; // 返回反转后 第k个节点 即这一组的最后一个节点
+    return last;  // 返回反转后 第k个节点 即这一组的最后一个节点
 }
 
-static ListNode *seek(ListNode *head, int k) { // 从head开始 找后面第k个节点 没有就返回nullptr
+static ListNode *seek(ListNode *head, int k) {  // 从head开始 找后面第k个节点 没有就返回nullptr
     for (int i = 0; i < k && head; ++i) {
         head = head->next;
     }
@@ -34,12 +34,33 @@ static ListNode *seek(ListNode *head, int k) { // 从head开始 找后面第k个
 static ListNode *reverseKGroup1(ListNode *head, int k) {
     ListNode tempHead(0, head);
     head = &tempHead;
-    while (seek(head, k)) { // 判断节点数量
-        head = reverse(head, k); // 反转链表 或许下一组的头节点
+    while (seek(head, k)) {       // 判断节点数量
+        head = reverse(head, k);  // 反转链表 或许下一组的头节点
     }
     return tempHead.next;
 }
 
-ListNode *LeetcodeTask25::solve(ListNode *head, int k) const {
-    return reverseKGroup1(head, k);
+static ListNode *reverseKGroup2(ListNode *head, int k) {
+    auto seek = [](ListNode *head, int k) -> ListNode * {
+        for (int i = 0; i < k && head; ++i, head = head->next);
+        return head;
+    };
+    auto reverse = [](ListNode *head, int k) -> ListNode * {
+        ListNode *p = head->next, *q = p;
+        for (int i = 0; i < k; ++i) {
+            ListNode *temp = p->next;
+            p->next = head->next;
+            head->next = p;
+            p = temp;
+        }
+        return q;
+    };
+    ListNode tempHead(0, head), *p = &tempHead, *q;
+    while ((q = seek(p, k)) != nullptr) {
+        ListNode *next = q->next;
+        (p = reverse(p, k))->next = next;
+    }
+    return tempHead.next;
 }
+
+ListNode *LeetcodeTask25::solve(ListNode *head, int k) const { return reverseKGroup2(head, k); }
