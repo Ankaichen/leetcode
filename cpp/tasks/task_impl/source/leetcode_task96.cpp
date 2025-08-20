7594a018069fa3cada1da25d4b3e3bfecafa531f
Ankaichen <2751551364akc@gmail.com>
2025-08-20 14:50:57 +0800

    review leetcode task 96

--- cpp/tasks/task_impl/source/leetcode_task96.cpp
+++ cpp/tasks/task_impl/source/leetcode_task96.cpp
@@ -1,32 +1,26 @@
 /**
-  ******************************************************************************
-  * @file           : leetcode_task96.cpp
-  * @author         : An Kaichen
-  * @brief          : None
-  * @attention      : None
-  * @date           : 25-6-19
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : leetcode_task96.cpp
+ * @author         : An Kaichen
+ * @brief          : None
+ * @attention      : None
+ * @date           : 25-6-19
+ ******************************************************************************
+ */
 
 #include "../include/leetcode_task96.h"
 
 static int numTrees1(int n) {
-    if (n < 3)
-        return n;
-    std::vector<int> dp(n + 1, 0);
-    dp[0] = 1;
-    dp[1] = 1;
-    dp[2] = 2;
-    for (int i = 3; i <= n; ++i) {
-        int count = 0;
-        for (int j = 0; j <= i - 1; ++j) {
-            count += dp[j] * dp[i - j - 1];
+    // 动态规划 dp[i]表示n=i时的结果
+    std::vector<int> dp(n + 1, 1);
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = 0;                            // 初始化为0 计算总和
+        for (int j = 1; j <= i; ++j) {        // j表示根节点
+            int left = j - 1, right = i - j;  // 计算根节点左右子树的节点数量
+            dp[i] += dp[left] * dp[right];    // 根据dp数组中保存的子树结果计算当前结果 将所有可能的根节点情况相加
         }
-        dp[i] = count;
     }
     return dp[n];
 }
 
-int LeetcodeTask96::solve(int n) const {
-    return numTrees1(n);
-}
+int LeetcodeTask96::solve(int n) const { return numTrees1(n); }
