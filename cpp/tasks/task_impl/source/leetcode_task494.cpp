7bf0202d6430f4fe5f1e96f9e5139b6c49c7b8d1
Ankaichen <2751551364akc@gmail.com>
2025-09-18 18:07:18 +0800

    add leetcode task 494

--- cpp/tasks/task_impl/source/leetcode_task494.cpp
+++ cpp/tasks/task_impl/source/leetcode_task494.cpp
@@ -25,9 +25,48 @@ static int findTargetSumWays1(const std::vector<int>& nums, int target) {
 
 static int findTargetSumWays2(const std::vector<int>& nums, int target) {
     // 动态规划
-    target = std::abs(target);
-    std::vector<std::vector<int>> dp(nums.size() + 1, std::vector<int>(target, 1));
-    for (int i = 0)
+    // 题目要求可以视为 将数组分为 left 和 right 两部分 满足 left - right = target; left + right = sum;
+    // 可知 right = left - target; -> left + (left - target) = sum; -> left = (sum + target) / 2;
+    // 此时 可将本题视为01背包问题 背包容量为(sum + target) / 2
+    target = std::accumulate(nums.begin(), nums.end(), std::abs(target));  // target 取绝对值 结果不变
+    if (target % 2 == 1) return 0;
+    target /= 2;  // 计算 (sum + target) / 2
+    std::vector<std::vector<int>> dp(target + 1, std::vector<int>(nums.size() + 1, 0));
+    dp[0][0] = 1;
+    int zeroCount = 1;
+    for (int i = 1; i <= nums.size(); ++i) {
+        if (nums[i - 1] == 0) zeroCount *= 2;  // 处理存在0的情况 出现一次0 则可能的次数翻倍
+        dp[0][i] = zeroCount;
+    }
+    for (int i = 1; i <= target; ++i) {
+        for (int j = 1; j <= nums.size(); ++j) {
+            dp[i][j] = dp[i][j - 1];
+            if (nums[j - 1] <= i) {
+                dp[i][j] += dp[i - nums[j - 1]][j - 1];
+            }
+        }
+    }
+    return dp[target][nums.size()];
 }
 
-int LeetcodeTask494::solve(const std::vector<int>& nums, int target) const { return findTargetSumWays2(nums, target); }
+static int findTargetSumWays3(const std::vector<int>& nums, int target) {
+    // 动态规划 一维数组
+    target = std::accumulate(nums.begin(), nums.end(), std::abs(target));
+    if (target % 2 == 1) return 0;
+    target /= 2;
+    std::vector<int> dp(target + 1, 0);
+    dp[0] = 1;
+    int zeroCount = 1;
+    for (int j = 1; j <= nums.size(); ++j) {
+        for (int i = target; i >= 1; --i) {
+            if (nums[j - 1] <= i) {
+                dp[i] += dp[i - nums[j - 1]];
+            }
+        }
+        if (nums[j - 1] == 0) zeroCount *= 2;
+        dp[0] = zeroCount;
+    }
+    return dp[target];
+}
+
+int LeetcodeTask494::solve(const std::vector<int>& nums, int target) const { return findTargetSumWays3(nums, target); }
