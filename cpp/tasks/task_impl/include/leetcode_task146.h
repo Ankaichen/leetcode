2096467ede6c606f9222092b617db36d87b4011e
ankaichen <2751551364akc@gmail.com>
2025-09-22 12:55:39 +0800

    add leetcode task 146

---
+++ cpp/tasks/task_impl/include/leetcode_task146.h
@@ -0,0 +1,23 @@
+/**
+  ******************************************************************************
+  * @file           : leetcode_task146.h
+  * @author         : An Kaichen
+  * @brief          : None
+  * @attention      : None
+  * @date           : 25-9-20
+  ******************************************************************************
+  */
+
+#ifndef LEETCODE_TASK146_H
+#define LEETCODE_TASK146_H
+
+#define using_LeetCodeTestCaseReader
+#define using_NormalTestResultProcessor
+
+#include "../../core/macro/define_task.h"
+
+DEFINE_TASK(LeetcodeTask146, "146.LRU缓存", std::vector<std::string>, const std::vector<std::string> &, const std::vector<std::vector<int>> &);
+
+#include "../../core/macro/end_define_task.h"
+
+#endif //LEETCODE_TASK130_H
