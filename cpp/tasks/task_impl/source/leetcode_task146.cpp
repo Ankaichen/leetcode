58ab3c34fc39c34068a350779a0865e9fd15cb2c
Ankaichen <2751551364akc@gmail.com>
2025-09-22 18:55:22 +0800

    add leetcode task 146

--- cpp/tasks/task_impl/source/leetcode_task146.cpp
+++ cpp/tasks/task_impl/source/leetcode_task146.cpp
@@ -15,9 +15,9 @@ class LRUCache1 {
         int value;
         int time;
     };
+
 public:
-    explicit LRUCache1(int capacity) : _capacity(capacity), _global_time(0), _data{} {
-    }
+    explicit LRUCache1(int capacity) : _capacity(capacity), _global_time(0), _data{} {}
 
     int get(int key) {
         auto iter = this->_data.find(key);
@@ -36,9 +36,7 @@ public:
     }
 
 private:
-    int update_global_time() {
-        return _global_time++;
-    }
+    int update_global_time() { return _global_time++; }
 
     void remove_time_min() {
         auto min_iter = this->_data.begin();
@@ -68,10 +66,55 @@ private:
     std::map<int, CacheItem> _data;
 };
 
-using LRUCache = LRUCache1;
+class LRUCache2 {
+public:
+    explicit LRUCache2(int capacity) : _capacity{capacity}, _data{}, _list{} {}
 
-std::vector<std::string>
-LeetcodeTask146::solve(const std::vector<std::string> &input1, const std::vector<std::vector<int>> &input2) const {
+    int get(int key) {
+        auto iter = _data.find(key);
+        if (iter == _data.end()) {
+            return -1;
+        } else {
+            this->move_iter(iter->second);
+            return iter->second->second;
+        }
+    }
+
+    void put(int key, int value) {
+        auto iter = _data.find(key);
+        if (iter == _data.end()) {
+            if (_data.size() >= _capacity) {
+                this->remove_item();
+            }
+            this->add_item(key, value);
+        } else {
+            iter->second->second = value;
+            this->move_iter(iter->second);
+        }
+    }
+
+private:
+    void add_item(int key, int value) {
+        _list.emplace_front(key, value);
+        _data.emplace(key, _list.begin());
+    }
+
+    void move_iter(std::list<std::pair<int, int>>::iterator iter) { _list.splice(_list.begin(), _list, iter); }
+
+    void remove_item() {
+        auto remove_key = _list.back().first;
+        _list.pop_back();
+        _data.erase(remove_key);
+    }
+
+private:
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> _data;
+    std::list<std::pair<int, int>> _list;
+    int _capacity;
+};
+
+std::vector<std::string> LeetcodeTask146::solve(const std::vector<std::string> &input1, const std::vector<std::vector<int>> &input2) const {
+    using LRUCache = LRUCache2;
     LRUCache *cache = nullptr;
     std::vector<std::string> result;
     for (int i = 0; i < input1.size(); ++i) {
