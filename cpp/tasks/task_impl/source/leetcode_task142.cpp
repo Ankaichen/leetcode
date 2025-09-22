6a956fd7944151fb70d517b9375f114e35ac0866
Ankaichen <2751551364akc@gmail.com>
2025-09-22 13:01:26 +0800

    review leetcode task 142

--- cpp/tasks/task_impl/source/leetcode_task142.cpp
+++ cpp/tasks/task_impl/source/leetcode_task142.cpp
@@ -1,18 +1,18 @@
 /**
-  ******************************************************************************
-  * @file           : leetcode_task142.cpp
-  * @author         : An Kaichen
-  * @brief          : None
-  * @attention      : None
-  * @date           : 25-6-19
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : leetcode_task142.cpp
+ * @author         : An Kaichen
+ * @brief          : None
+ * @attention      : None
+ * @date           : 25-6-19
+ ******************************************************************************
+ */
 
 #include "../include/leetcode_task142.h"
 
 static ListNode *detectCycle1(ListNode *head) {
-    std::unordered_set<ListNode*> node_set;
-    while(head && !node_set.count(head)) {
+    std::unordered_set<ListNode *> node_set;
+    while (head && !node_set.count(head)) {
         node_set.insert(head);
         head = head->next;
     }
@@ -35,6 +35,26 @@ static ListNode *detectCycle2(ListNode *head) {
     return p;
 }
 
+static ListNode *detectCycle3(ListNode *head) {
+    // a 环前长度 b 环长度 x1 交汇点前环内长度 x2 交汇点后环内长度
+    // 2 * (a + x1) = (a + n * b + x1)
+    //            a = n * b - x1
+    //            a = (n - 1) * b + x2
+    if (head == nullptr) return nullptr;
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) break;
+    }
+    if (!fast || !fast->next) return nullptr;
+    while (head != slow) {
+        head = head->next;
+        slow = slow->next;
+    }
+    return head;
+}
+
 int LeetcodeTask142::solve(ListNode *head, int pos) const {
     if (head && pos != -1) {
         ListNode *tail = head, *p = head;
@@ -42,7 +62,7 @@ int LeetcodeTask142::solve(ListNode *head, int pos) const {
         for (int i = 0; i < pos; ++i, p = p->next);
         tail->next = p;
     }
-    ListNode *resP = detectCycle2(head);
+    ListNode *resP = detectCycle3(head);
     if (head && pos != -1) {
         ListNode *p = head;
         for (int i = 0; i <= pos; ++i, p = p->next) {
